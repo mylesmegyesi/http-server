bd85f73650f6f63d2b49c6ec0a20af10bc9ae0c8
Myles Megyesi <mylesmegyesi@me.com>
2011-12-03 23:05:02 -0600

    command-line arg input

--- src/HttpServer/Main.java
+++ src/HttpServer/Main.java
@@ -7,9 +7,7 @@ import HttpServer.Utility.FileInfo;
 import SocketServer.RequestHandlerFactory;
 import SocketServer.SocketServerShutdownHandler;
 import SocketServer.Utility.Logging;
-import com.sun.tools.javac.resources.javac;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -19,16 +17,28 @@ import java.util.logging.Logger;
  * Author: Myles Megyesi
  */
 public class Main {
+
+
+    private static void printUsage() {
+        System.out.println("Usage: -p <port> -d <directory>");
+    }
+
     public static void main(String[] args) {
-        String dir = "./public";
         int port = 8080;
+        String directory = ".";
+
+        if (args.length == 4) {
+           port = Integer.parseInt(args[1]);
+            directory = args[3];
+        }
+
         FileInfo fileInfo = new FileInfo();
         Logger logger = Logging.getLoggerAndSetLevel(Main.class.getName(), Level.ALL);
 
         // Setup responders
         List<SocketServer.Responder> responders = new ArrayList<SocketServer.Responder>();
-        responders.add(new Directory(dir, fileInfo, logger));
-        responders.add(new File(dir,fileInfo, logger));
+        responders.add(new Directory(directory, fileInfo, logger));
+        responders.add(new File(directory, fileInfo, logger));
         responders.add(new NotFound());
 
         RequestParser requestParser = new RequestParser();
